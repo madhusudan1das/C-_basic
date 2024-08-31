744cf46cfa5b4425f2b2b7765dab9790f7b52065
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-08-31 22:23:22 +0530

    c++

---
+++ area_of_traingle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    double base,height,area;
+    std::cout<<"enter base and height: ";
+    std::cin>>base>>height;
+    area=(base*height)/2;
+    std::cout<<"area is"<<area;
+    return 0;
+}
