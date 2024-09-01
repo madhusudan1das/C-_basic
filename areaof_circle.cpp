8b6c4c0976458e8df267f78d2914a2c06a0b0c95
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-09-01 07:38:20 +0530

    Add files via upload

---
+++ areaof_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double r;
+    cout<<"enter radius: ";
+    cin>>r;
+    double area=3.1425f;
+    area*=r;
+    cout<<"area is: "<<area;
+    return 0;
+}
