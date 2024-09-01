808398ffc7e66e9292014b6c60ef0a27debc6b77
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-09-01 07:36:56 +0530

    c++

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double num1, num2, sum;
+
+    // Prompt the user to enter two numbers
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // Calculate the sum of the two numbers
+    sum = num1 / num2;
+
+    // Display the result
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." <<std::endl ;
+
+    return 0;
+}
