0d44db617823b9a62d2ef3573ecf39457a4d2e9c
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-09-11 22:03:51 +0530

    c++

---
+++ max_of_2number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+    int x,y;
+    std::cout<<"enter the x and y number: ";
+    std::cin>>x>>y;
+    if (x>y){
+        std::cout<<"x is greater: "<<x << std::endl;
+    }else{
+        std::cout<<"y is greater: "<<y << std::endl;
+        }
+        
+        return 0;
+    }
+
