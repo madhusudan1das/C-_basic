23f0c509c3801ba7c12b4cfc10b11ccc0eafff41
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-09-14 23:33:06 +0530

    c++
    do while loop example

---
+++ do_while_loop_example.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i = 1; // Initialization expression
+
+	do {
+		// loop body
+		cout << "Hello World\n";
+
+		// update expression
+		i++;
+
+	} while (i <=8); // test expression
+
+	return 0;
+}
