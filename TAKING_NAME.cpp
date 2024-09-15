0047c31a5340be2633b10c85e7b045c70bf11de0
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-09-15 20:18:29 +0530

    c++

---
+++ TAKING_NAME.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+using namespace std;
+string name; 
+cout<<"MAY I KNOW YOUR NAME: ";
+ cin>>name;//we can use this ->getline(cin,name);
+cout<<"welcome: "<<name;
+return 0;
+}
