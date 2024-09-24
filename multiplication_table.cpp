1e16170aba34480ba0528e0864a07d239dc5ceb5
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2024-09-24 11:50:25 +0530

    c++

---
+++ multiplication_table.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"enter number n: ";
+    cin>>n;
+    for(i=1;i<=10;i++){
+            cout<<n<<"*"<<i<<"="<<n*i<<endl;
+
+    }
+    return 0;
+}
