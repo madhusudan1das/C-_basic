14af240fd540a1f843232a6208a8ac0d9cc933e9
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-01-18 09:36:59 +0530

    c++

---
+++ mulitpilcation_table.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i=1;
+    cout<<"enter number n: ";
+    cin>>n;
+    for(i=1;i<=10;i++){
+        cout<<n<<" x "<< i<<"="<<i*n;
+        cout<<endl; 
+    }
+    return 0;
+}
