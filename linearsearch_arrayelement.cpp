71674e1b40d89b580f5aa5c3a82518df13a35cfe
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-01-21 18:02:42 +0530

    c++

---
+++ linearsearch_arrayelement.cpp
@@ -0,0 +1,68 @@
+  /*
+  // Program for Linear Search
+#include <iostream>
+using namespace std;
+int main()
+{
+int A[10]={2,4,6,8,12,3,5,7,9};
+int key;
+ cout<<"Enter a Key element ";
+ cin>>key;
+for(int i=0;i<10;i++)
+ {
+ if(key==A[i])
+ {
+ cout<<"The Key element is found at "<<i<<endl;
+ exit(0);
+ }
+ }
+ cout<<"Key element not found"<<endl;
+return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a[10],n=10,i;
+    cout<<"enter number: ";
+     for(i=0;i<10;i++){
+    cin>>a[i];
+     }
+     int key=0;
+     cout<<"enter key: ";
+     cin>>key;
+     for(i=0;i<10;i++){
+        if(key==a[i]){
+            cout<<"key found at"<<i;
+            return 0;
+        }
+     }
+    cout<<"not found";
+
+}
+*/
+
+  
+  // Program for Linear Search
+#include <iostream>
+using namespace std;
+int main()
+{
+int A[10]={2,4,6,8,12,3,5,7,9};
+int key;
+ cout<<"Enter a Key element ";
+ cin>>key;
+for(int i=0;i<10;i++)
+ {
+ if(key==A[i])
+ {
+ cout<<"The Key element is found at "<<i<<endl;
+ exit(0);
+ }
+ }
+ cout<<"Key element not found"<<endl;
+return 0;
+}
+
+
