fdf7f49495fa0abb2863979dc0fa21e8b0266e28
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-02-05 22:19:48 +0530

    c++

---
+++ leetcode1.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> rows(9), cols(9), boxes(9);
+        
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char num = board[i][j];
+                if (num == '.') continue;
+                
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                
+                if (rows[i].count(num) || cols[j].count(num) || boxes[boxIndex].count(num)) {
+                    return false;
+                }
+                
+                rows[i].insert(num);
+                cols[j].insert(num);
+                boxes[boxIndex].insert(num);
+            }
+        }
+        
+        return true;
+    }
+};
